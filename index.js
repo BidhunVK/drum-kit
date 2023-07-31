cc8ed946bbd917cabed715c047e887492d242c8d
Bidhun V K <bidhunvaikkath@gmail.com>
2023-07-31 19:24:23 +0530

    detect the pressed button and produce sound, works in keyboard as well.

---
+++ index.js
@@ -0,0 +1,68 @@
+
+//Detect button press
+
+var numOfButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numOfButtons; i++) {
+
+    document.querySelectorAll("button")[i].addEventListener("click", function () {
+
+        var buttonInnerHTML = this.innerHTML;
+        makeSound(buttonInnerHTML);
+
+        
+    });
+}
+
+// Detect keyboard press
+document.addEventListener("keydown", function(event){
+    
+    makeSound(event.key);
+    
+    
+})
+
+function makeSound(key){
+    switch (key) {
+        case "w":
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+        case "l":
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+        default: console.log(buttonInnerHTML);
+
+    }
+}
+
+
+
+
+// function handleClick(){
+//     alert("I got clicked");
+// }
+
+// var audio = new Audio('sounds/kick-bass.mp3');
+// audio.play();
